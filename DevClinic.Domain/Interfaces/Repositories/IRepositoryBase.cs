8c00bb688fc79153ad28fda34a3f7afd59e05fea
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-16 23:02:03 -0300

    Criado as interfaces de Repositorio e Service Basic

---
+++ DevClinic.Domain/Interfaces/Repositories/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using DevClinic.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevClinic.Domain.Interfaces.Repositories
+{
+    public interface IRepositoryBase<TEntity> : IDisposable where TEntity : BaseEntity
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int id);
+        IList<TEntity> GetAll();
+        TEntity GetById(int id);
+    }
+}
---
+++ DevClinic.Domain/Interfaces/Services/IServiceBase.cs
@@ -0,0 +1,20 @@
+﻿using DevClinic.Domain.Entities;
+using FluentValidation;
+
+namespace DevClinic.Domain.Interfaces.Services
+{
+    public interface IServiceBase<TEntity> where TEntity : BaseEntity
+    {
+        TOutputModel Add<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
+            where TOutputModel : class
+            where TInputModel : class
+            where TValidator : AbstractValidator<TEntity>;
+        TOutputModel Update<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
+            where TOutputModel : class
+            where TInputModel : class
+            where TValidator : AbstractValidator<TEntity>;
+        void Delete(int id);
+        IEnumerable<TOutputModel> GetAll<TOutputModel>() where TOutputModel : class;
+        TOutputModel GetById<TOutputModel>(int id) where TOutputModel : class;
+    }
+}
