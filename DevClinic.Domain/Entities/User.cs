9f4e273382a74715d415e0d4c423b442992f1553
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-16 22:05:55 -0300

    Adicionar arquivos de projeto.

---
+++ DevClinic.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevClinic.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string CPF { get; private set; }
+        public char Sexo { get; private set; }
+        public bool Active { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    }
+}
