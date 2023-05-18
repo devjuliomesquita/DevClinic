7e3cd8ada38c751ba1cadee9f920fbcdb543bdb2
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-18 00:29:42 -0300

    Controller do cliente criada

--- DevClinic.API/Controllers/ClientController.cs
+++ DevClinic.API/Controllers/ClientController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DevClinic.API.DTO.InputModels.Client;
+using DevClinic.API.DTO.ViewModels.Client;
+using DevClinic.Domain.Interfaces.Services;
+using DevClinic.Services.Validators;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevClinic.API.Controllers
@@ -7,5 +11,71 @@ namespace DevClinic.API.Controllers
     [ApiController]
     public class ClientController : ControllerBase
     {
+        private readonly IClientService _clientService;
+        public ClientController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            return
+                Execute(() => _clientService.GetAll<Client_ViewModel>());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            if (id == 0) return NotFound();
+            return
+                Execute(() => _clientService.GetById<ClientStandard_ViewModel>(id));
+        }
+        [HttpGet("{id}/Details")]
+        public IActionResult GetDetails(int id)
+        {
+            if (id == 0) return NotFound();
+            return
+                Execute(() => _clientService.GetDetails<ClientDetails_ViewModel>(id));
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0) return NotFound();
+            Execute(() =>
+            {
+                _clientService.Delete(id);
+                return true;
+            });
+            return new ContentResult();
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] ClientCreate_InputModel inputModel)
+        {
+            if (inputModel == null) return NoContent();
+            return
+                Execute(() => _clientService.Add<ClientCreate_InputModel, Client_ViewModel, ClientValidator>(inputModel));
+        }
+        [HttpPut]
+        public IActionResult Update([FromBody] ClientUpdate_InputModel inputModel)
+        {
+            if (inputModel == null) return NoContent();
+            return
+                Execute(() => _clientService.Update<ClientUpdate_InputModel, Client_ViewModel, ClientValidator>(inputModel));
+        }
+        
+
+        //Criando o Método de execução
+        private IActionResult Execute(Func<object> func)
+        {
+            try
+            {
+                var result = func();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
