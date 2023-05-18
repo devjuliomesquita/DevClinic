325cea521f071eedbbe43dfed38e130c26a17c73
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-18 00:09:19 -0300

    Salvado

--- DevClinic.API/DTO/ViewModels/Client/ClientDetails_ViewModel.cs
+++ DevClinic.API/DTO/ViewModels/Client/ClientDetails_ViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class ClientDetails_ViewModel
     {
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? CPF { get; set; }
         public string? Email { get; set; }
--- DevClinic.API/DTO/ViewModels/Client/ClientStandard_ViewModel.cs
+++ DevClinic.API/DTO/ViewModels/Client/ClientStandard_ViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class ClientStandard_ViewModel
     {
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? CPF { get; set; }
         public string? Email { get; set; }
