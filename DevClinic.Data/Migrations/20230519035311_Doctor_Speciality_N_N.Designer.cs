40275c2426edc49a42b3d4e1ba779cd582bae6f2
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-19 00:56:14 -0300

    Migration e update-database

--- DevClinic.Data/Migrations/20230519035311_Doctor_Speciality_N_N.Designer.cs
+++ DevClinic.Data/Migrations/20230519035311_Doctor_Speciality_N_N.Designer.cs
@@ -4,6 +4,7 @@ using DevClinic.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevClinic.Data.Migrations
 {
     [DbContext(typeof(DevClinic_Context))]
-    partial class DevClinic_ContextModelSnapshot : ModelSnapshot
+    [Migration("20230519035311_Doctor_Speciality_N_N")]
+    partial class Doctor_Speciality_N_N
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
