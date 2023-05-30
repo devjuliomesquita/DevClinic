9fcc6be3bb955b7ab212bb090a2fd0d33c948315
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-30 00:28:19 -0300

    salvado

--- DevClinic.Data/Repository/Repositorybase.cs
+++ DevClinic.Data/Repository/Repositorybase.cs
@@ -2,11 +2,7 @@
 using DevClinic.Domain.Entities;
 using DevClinic.Domain.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DevClinic.Data.Repository
 {
