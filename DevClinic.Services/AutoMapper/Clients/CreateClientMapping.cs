95d16a81105f9749f8e454a0302ed75f01676263
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-31 02:07:52 -0300

    criado o token de cadastro

--- DevClinic.Services/AutoMapper/Clients/CreateClientMapping.cs
+++ DevClinic.Services/AutoMapper/Clients/CreateClientMapping.cs
@@ -12,8 +12,13 @@ namespace DevClinic.Services.AutoMapper.Clients
             CreateMap<ClientCreate_InputModel, Client>()
                 .ForMember(c => c.CreatedAt, c => c.MapFrom(c => DateTime.Now.Date))
                 .ForMember(c => c.Active, c => c.MapFrom(c => true))
-                .ForMember(c => c.Register, c => c.MapFrom(c => Guid.NewGuid().ToString("N").ToUpper())) ;
+                .ForMember(c => c.Register, c => c.MapFrom(c => Token())) ;
 
+        }
+        private string Token()
+        {
+            var token = Guid.NewGuid().ToString("N").ToUpper()[..6];
+            return token;
         }
     }
 }
