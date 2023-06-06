2c3c2b25efcd2b9f6b660c29feedb2e398b86ea7
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-06-06 02:31:19 -0300

    realizado a documentação da contrller e da inputmodel Create

--- DevClinic.API/Controllers/ClientController.cs
+++ DevClinic.API/Controllers/ClientController.cs
@@ -1,5 +1,6 @@
 ﻿
 using DevClinic.Domain.DTO.Clients;
+using DevClinic.Domain.Entities;
 using DevClinic.Manager.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,25 +15,51 @@ namespace DevClinic.API.Controllers
         {
             _clientService = clientService;
         }
+        /// <summary>
+        /// Retorna a lista de clientes cadastrado na base de dados.
+        /// </summary>
         [HttpGet]
+        [ProducesResponseType(typeof(Client), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAll() 
         {
             return
                 Ok(await _clientService.GetAllAsync());
         }
+        /// <summary>
+        /// Retorna um cliente que é buscado pelo ID.
+        /// </summary>
+        /// <param name="id" example="2">Id do cliente.</param>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Client), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetById(int id)
         {
             return
                 Ok(await _clientService.GetByIdAsync(id));
         }
+        /// <summary>
+        /// Excluir cliente pelo ID.
+        /// </summary>
+        /// <param name="id" example="2">Id do CLiente.</param>
+        /// <remarks>Ao excluir um cliente o mesmo será removido permanentemente da base de dados.</remarks>
         [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(Client), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Delete(int id)
         {
             await _clientService.DeleteAsync(id);
             return NoContent();
         }
+        /// <summary>
+        /// Inserir um novo cliente.
+        /// </summary>
+        /// <param name="inputModel"></param>
         [HttpPost]
+        [ProducesResponseType(typeof(Client), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Create(CreateClient_InputModel inputModel)
         {
             var clientNew = await _clientService.AddAsync(inputModel);
@@ -40,7 +67,14 @@ namespace DevClinic.API.Controllers
                 CreatedAtAction(nameof(GetById), new {id = clientNew.Id}, clientNew);
                 
         }
+        /// <summary>
+        /// Atualizãr os dados cadastratis de um novo cliente.
+        /// </summary>
+        /// <param name="inputModel"></param>
         [HttpPut]
+        [ProducesResponseType(typeof(Client), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Update(UpdateClient_InputModel inputModel)
         {
             var clientUpdate = await _clientService.UpdateAsync(inputModel);
--- DevClinic.Domain/DTO/Clients/CreateClient_InputModel.cs
+++ DevClinic.Domain/DTO/Clients/CreateClient_InputModel.cs
@@ -2,15 +2,50 @@
 
 namespace DevClinic.Domain.DTO.Clients
 {
+    /// <summary>
+    /// Objeto complexo utlizado para a criação de novos clientes.
+    /// </summary>
     public class CreateClient_InputModel
     {
+        /// <summary>
+        /// Nome do cliente.
+        /// </summary>
+        /// <example>Júlio César de Mesquita Camilo Martins</example>
         public string? Name { get; set; }
+        /// <summary>
+        /// CPF do cliente.
+        /// </summary>
+        /// <example>12345678945</example>
         public string? CPF { get; set; }
+        /// <summary>
+        /// E-mail do cliente.
+        /// </summary>
+        /// <example>juliocesarmcamilo@gmail.com</example>
         public string? Email { get; set; }
+        /// <summary>
+        /// Telefone do cliente.
+        /// </summary>
+        /// <example>999886020</example>
         public string? Phone { get; set; }
+        /// <summary>
+        /// Sexo do cliente.
+        /// </summary>
+        /// <example>M</example>
         public char Sexo { get; set; }
+        /// <summary>
+        /// Data de nascimento do cliente.
+        /// </summary>
+        /// <example>1994/11/12</example>
         public DateTime BirthDate { get; set; }
+        /// <summary>
+        /// Código de registro do cliente.
+        /// </summary>
+        /// <example>abc123</example>
         public string? Register { get; set; }
+        /// <summary>
+        /// Tipo de plano do cliente.
+        /// </summary>
+        /// <example>Credenciada</example>
         public string? Plan { get; set; }
     }
 }
