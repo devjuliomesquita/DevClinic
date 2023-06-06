5e431b6a2816ba3c82a98d37e815e62988fa6cee
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-06-05 23:58:29 -0300

    Crud ok

--- DevClinic.Data/Context/DevClinic_Context.cs
+++ DevClinic.Data/Context/DevClinic_Context.cs
@@ -1,11 +1,7 @@
 ﻿using DevClinic.Data.Maping;
 using DevClinic.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DevClinic.Data.Context
 {
