03e94471d1162df7c59c1b50b01d9e34e80136b6
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-06-05 21:16:11 -0300

    Criado alguns serviços e instalado as bibliotecas de automapper e fluent validation

---
+++ DevClinic.Manager/Implementation/ClientService.cs
@@ -0,0 +1,24 @@
+﻿using DevClinic.Domain.Entities;
+using DevClinic.Manager.Interfaces.Repository;
+using DevClinic.Manager.Interfaces.Services;
+
+
+namespace DevClinic.Manager.Implementation
+{
+    public class ClientService : ServiceBase<Client>, IClientService
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepositoryBase<Client> _repositoryBase;
+        private readonly IClientRepository _clientRepository;
+        public ClientService(
+            IMapper mapper,
+            IRepositoryBase<Client> repositoryBase,
+            IClientRepository clientRepository) : base(mapper, repositoryBase)
+        {
+            _mapper = mapper;
+            _repositoryBase = repositoryBase;
+            _clientRepository = clientRepository;
+        }
+
+    }
+}
---
+++ DevClinic.Manager/Implementation/ServiceBase.cs
@@ -0,0 +1,44 @@
+﻿using DevClinic.Domain.Entities;
+using DevClinic.Manager.Interfaces.Repository;
+using DevClinic.Manager.Interfaces.Services;
+
+namespace DevClinic.Manager.Implementation
+{
+    public class ServiceBase<TEntity> : IServiceBase<TEntity> where TEntity : BaseEntity
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepositoryBase<TEntity> _repositoryBase;
+        public ServiceBase(IMapper mapper, IRepositoryBase<TEntity> repositoryBase)
+        {
+            _mapper = mapper;
+            _repositoryBase = repositoryBase;
+        }
+
+        public async Task<TEntity> AddAsync<TInputModel>(TInputModel inputModel) where TInputModel : class
+        {
+            var entity = _mapper.Map<TEntity>(inputModel);
+            return await _repositoryBase.AddAsync(entity);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _repositoryBase.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _repositoryBase.GetAllAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _repositoryBase.GetByIdAsync(id);
+        }
+
+        public async Task<TEntity> UpdateAsync<TInputModel>(TInputModel inputModel) where TInputModel : class
+        {
+            var entity = _mapper.Map<TEntity>(inputModel);
+            return await _repositoryBase.UpdateAsync(entity);
+        }
+    }
+}
