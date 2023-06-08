b4203d0f7cfea40cd25bd8140042867fd790525e
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-06-08 00:15:09 -0300

    Criado a DTO que irá notificar os erros.

---
+++ DevClinic.Domain/DTO/Error/ErrorResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevClinic.Domain.DTO.Error
+{
+    public class ErrorResponse
+    {
+        public ErrorResponse(string id)
+        {
+            Id = id;
+            DateError = DateTime.Now;
+            Message = "Erro inesperado";
+        }
+        public string Id { get; set; }
+        public DateTime DateError { get; set; }
+        public string Message { get; set; }
+    }
+}
