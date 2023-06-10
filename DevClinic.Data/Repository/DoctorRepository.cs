40e19321a23a9c72cc75f1bc4b6b87ff4ee66075
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-06-10 05:51:41 -0300

    Adicionado ao contexto de dados o repositorio do Doctor.

---
+++ DevClinic.Data/Repository/DoctorRepository.cs
@@ -0,0 +1,64 @@
+﻿using DevClinic.Data.Context;
+using DevClinic.Domain.Entities;
+using DevClinic.Manager.Interfaces.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevClinic.Data.Repository
+{
+    public class DoctorRepository : Repositorybase<Doctor>, IDoctorRepository
+    {
+        private readonly DevClinic_Context _context;
+        public DoctorRepository(DevClinic_Context context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Doctor> CreateDoctorAsync(Doctor doctor)
+        {
+            await _context.Doctors.AddAsync(doctor);
+            await CreateDoctorSpecialiaty(doctor);
+            await _context.SaveChangesAsync();
+            return doctor; 
+        }
+
+        public async Task<IEnumerable<Doctor>> GetAllDoctorsAsync()
+        {
+            return
+                await _context.Doctors.Include(d => d.Specialities).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Doctor> GetDoctorByIdAsync(int id)
+        {
+            return
+                await _context.Doctors.Include(d => d.Specialities).AsNoTracking().SingleOrDefaultAsync(d => d.Id == id);
+        }
+
+        public async Task<Doctor> UpdateDoctorAync(Doctor doctor)
+        {
+            var queryDoctor = await _context.Doctors.Include(d => d.Specialities).SingleOrDefaultAsync(d => d.Id == doctor.Id);
+            if (queryDoctor == null) return null;
+            _context.Entry(queryDoctor).CurrentValues.SetValues(doctor);
+            await UpdateDoctorSpeciality(doctor, queryDoctor);
+            await _context.SaveChangesAsync();
+            return queryDoctor;
+        }
+
+        private async Task CreateDoctorSpecialiaty(Doctor doctor)
+        {
+            foreach (var speciality in doctor.Specialities)
+            {
+                var querySpeciality = await _context.Specialities.AsNoTracking().FirstAsync(s => s.Id == speciality.Id);
+                _context.Entry(speciality).CurrentValues.SetValues(querySpeciality);
+            }
+        }
+        private async Task UpdateDoctorSpeciality(Doctor doctor, Doctor queryDoctor)
+        {
+            queryDoctor.Specialities.Clear();
+            foreach (var speciality in doctor.Specialities)
+            {
+                var querySpeciality = await _context.Specialities.FindAsync(speciality.Id);
+                queryDoctor.Specialities.Add(querySpeciality);
+            }
+        }
+    }
+}
---
+++ DevClinic.Manager/Interfaces/Repository/IDoctorRepository.cs
@@ -0,0 +1,12 @@
+﻿using DevClinic.Domain.Entities;
+
+namespace DevClinic.Manager.Interfaces.Repository
+{
+    public interface IDoctorRepository : IRepositoryBase<Doctor>
+    {
+        Task<Doctor> GetDoctorByIdAsync(int id);
+        Task<IEnumerable<Doctor>> GetAllDoctorsAsync();
+        Task<Doctor> CreateDoctorAsync(Doctor doctor);
+        Task<Doctor> UpdateDoctorAync(Doctor doctor);
+    }
+}
